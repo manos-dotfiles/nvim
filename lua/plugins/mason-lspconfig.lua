def130984adf290b62c9b91555f14c588495f554
manoskamarianos <manospk160@gmail.com>
2025-07-18 20:01:49 +0300

    fix(mason-lspconfig): remove old setup handlers method and update setup

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,8 +2,7 @@ return {
   "williamboman/mason-lspconfig.nvim",
   dependencies = { "mason.nvim" },
   config = function ()
-    require("mason-lspconfig").setup()
-    require("mason-lspconfig").setup_handlers({
+    require("mason-lspconfig").setup({
       function(server_name)
         local capabilities = require('blink.cmp').get_lsp_capabilities()
         require("lspconfig")[server_name].setup({ capabilities = capabilities })
