019f69ca0ffc7c2a881de4c33d7e8d258135245a
manoskamarianos <manospk160@gmail.com>
2025-12-29 13:43:05 +0200

    fix(alpha-dash): resolve #2

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -1,71 +1,69 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  dependencies = { "RchrdAriza/nvim-web-devicons" },
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    dependencies = { "RchrdAriza/nvim-web-devicons" },
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
 
-    local time = os.date("%H:%M")
-    local date = os.date("%a %d %b")
-    local v = vim.version()
-    local version = " v" .. v.major .. "." .. v.minor .. "." .. v.patch
-    local stats = require("lazy").stats()
-    local cputime = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-    local ms = cputime .. " ms"
-    local statistics = "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-    -- Set header
-    dashboard.section.header.val = {
-      "       ███╗░░██╗██╗░░░██╗██╗███╗░░░███╗",
-      "       ████╗░██║██║░░░██║██║████╗░████║",
-      "       ██╔██╗██║╚██╗░██╔╝██║██╔████╔██║",
-      "       ██║╚████║░╚████╔╝░██║██║╚██╔╝██║",
-      "       ██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║",
-      "       ╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
-      "",
-      "                 █▀█ █▄░█  █▀▄▀█ █▄█            ",
-      "                 █▄█ █░▀█  █░▀░█ ░█░            ",
-      "",
-      "       ░██╗░░░░░░░██╗░█████╗░██╗░░░██╗",
-      "       ░██║░░██╗░░██║██╔══██╗╚██╗░██╔╝",
-      "       ░╚██╗████╗██╔╝███████║░╚████╔╝░",
-      "       ░░████╔═████║░██╔══██║░░╚██╔╝░░",
-      "       ░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░",
-      "       ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░",
-    }
+        local time = os.date("%H:%M")
+        local date = os.date("%a %d %b")
+        local v = vim.version()
+        local version = " v" .. v.major .. "." .. v.minor .. "." .. v.patch
+        local stats = require("lazy").stats()
+        local cputime = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        local ms = cputime .. " ms"
+        local statistics = "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        local cwd = os.cwd
 
-    dashboard.section.buttons.val = {
-      dashboard.button("n", "   New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
-      dashboard.button("e", "   File Explorer", ":cd $HOME | NvimTreeFocus<CR>"),
-      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("c", "   Nvim Configuration", ":e ~/.config/nvim/init.lua<CR>"),
-      dashboard.button("R", "󱘞   Ripgrep", ":Telescope live_grep<CR>"),
-      dashboard.button("q", "󰗼   Quit", ":qa<CR>"),
-    }
+        -- Set header
+        dashboard.section.header.val = {
+            "       ███╗░░██╗██╗░░░██╗██╗███╗░░░███╗",
+            "       ████╗░██║██║░░░██║██║████╗░████║",
+            "       ██╔██╗██║╚██╗░██╔╝██║██╔████╔██║",
+            "       ██║╚████║░╚████╔╝░██║██║╚██╔╝██║",
+            "       ██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║",
+            "       ╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
+            "",
+            "                 █▀█ █▄░█  █▀▄▀█ █▄█            ",
+            "                 █▄█ █░▀█  █░▀░█ ░█░            ",
+            "",
+            "       ░██╗░░░░░░░██╗░█████╗░██╗░░░██╗",
+            "       ░██║░░██╗░░██║██╔══██╗╚██╗░██╔╝",
+            "       ░╚██╗████╗██╔╝███████║░╚████╔╝░",
+            "       ░░████╔═████║░██╔══██║░░╚██╔╝░░",
+            "       ░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░",
+            "       ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░",
+        }
 
-    function centerText(text, width)
-      local totalPadding = width - #text
-      local leftPadding = math.floor(totalPadding / 2)
-      local rightPadding = totalPadding - leftPadding
-      return string.rep(" ", leftPadding) .. text .. string.rep(" ", rightPadding)
-    end
+        dashboard.section.buttons.val = {
+            dashboard.button("n", "   New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("f", "󰮗   Find file", ":cd $PWD | Telescope find_files<CR>"),
+            dashboard.button("e", "   File Explorer", ":cd $PWD | NvimTreeFocus<CR>"),
+            dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("c", "   Nvim Configuration", ":cd $HOME/.config/nvim | :e ~/.config/nvim/init.lua<CR>"),
+            dashboard.button("R", "󱘞   Ripgrep", ":Telescope live_grep<CR>"),
+            dashboard.button("q", "󰗼   Quit", ":qa<CR>"),
+        }
 
-    dashboard.section.footer.val = {
-      centerText("Kaizoku Ou Ni Ore Wa Naru", 50),
-      " ",
-      -- centerText("NvimOnMy_Way❤️", 50),
-      -- " ",
-      centerText(date, 50),
-      centerText(time, 50),
-      centerText(version, 50),
-      centerText(statistics, 50),
-    }
+        function centerText(text, width)
+            local totalPadding = width - #text
+            local leftPadding = math.floor(totalPadding / 2)
+            local rightPadding = totalPadding - leftPadding
+            return string.rep(" ", leftPadding) .. text .. string.rep(" ", rightPadding)
+        end
 
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
+        dashboard.section.footer.val = {
+            centerText(date, 50),
+            centerText(time, 50),
+            centerText(version, 50),
+            centerText(statistics, 50),
+        }
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
 }
